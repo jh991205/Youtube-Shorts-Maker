3ea5363002e753c58e8c25ac4eba4617df0de414
jh991205 <jiadihuang19991205@hotmail.com>
2024-07-22 22:04:25 -0400

    youtube_upload_automation

--- video_creator.py
+++ video_creator.py
@@ -128,3 +128,43 @@ else:
 
 final_video_check.close()
 final_video.close()
+
+
+import os
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+scopes = ["https://www.googleapis.com/auth/youtube.upload"]
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+api_service_name = "youtube"
+api_version = "v3"
+client_secrets_file = "secret.json"
+
+flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    client_secrets_file, scopes)
+credentials = flow.run_local_server(port=0)
+youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, credentials=credentials)
+
+request = youtube.videos().insert(
+    part="snippet,status",
+    body={
+        "snippet": {
+            "categoryId": "22",
+            "description": f"{content} #aitah #aita #shorts",
+            "title": title + "#shorts",
+            "defaultLanguage": "en",
+        },
+        "status": {
+            "privacyStatus": "public",
+            "madeForKids": False
+        }
+    },
+    media_body=googleapiclient.http.MediaFileUpload("generated/final_short.mp4")
+)
+response = request.execute()
+
+print(response)
